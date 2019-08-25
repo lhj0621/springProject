0456f4c247ffe447b018ca274ce89626240aaee2
lhj0621 <lhj0621@gmail.com>
2019-08-25 20:44:16 +0900

    creating product

---
+++ src/main/java/iducs/springboot/board/domain/Product.java
@@ -0,0 +1,32 @@
+package iducs.springboot.board.domain;
+
+public class Product {
+	private long id;
+	private String name;
+	private int price;
+	private String image;
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getPrice() {
+		return price;
+	}
+	public void setPrice(int price) {
+		this.price = price;
+	}
+	public String getImage() {
+		return image;
+	}
+	public void setImage(String image) {
+		this.image = image;
+	}
+}
