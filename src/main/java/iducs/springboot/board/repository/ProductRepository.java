5045b7a61222dcbe44035a43ee6cb4812c3bec4f
lhj0621 <lhj0621@gmail.com>
2019-08-27 21:40:08 +0900

    create ProductRepository

---
+++ src/main/java/iducs/springboot/board/repository/ProductRepository.java
@@ -0,0 +1,13 @@
+package iducs.springboot.board.repository;
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import iducs.springboot.board.entity.ProductEntity;
+
+
+public interface ProductRepository 
+	extends JpaRepository<ProductEntity, Long> {	
+	ProductEntity findByName(String name);
+	
+}
