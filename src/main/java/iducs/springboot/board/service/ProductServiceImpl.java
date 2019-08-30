c0618a5896dec9213952faf8f6398795e0c349dc
lhj0621 <lhj0621@gmail.com>
2019-08-30 22:20:16 +0900

    update ProductServiceImpl

--- src/main/java/iducs/springboot/board/service/ProductServiceImpl.java
+++ src/main/java/iducs/springboot/board/service/ProductServiceImpl.java
@@ -1,11 +1,14 @@
 package iducs.springboot.board.service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import iducs.springboot.board.domain.Product;
+import iducs.springboot.board.entity.ProductEntity;
+import iducs.springboot.board.exception.ResourceNotFoundException;
 import iducs.springboot.board.repository.ProductRepository;
 
 @Service
@@ -13,17 +16,28 @@ public class ProductServiceImpl implements ProductService {
 
 	@Autowired
 	ProductRepository repository;
-	
+
 	@Override
 	public Product getProductByName(String name) {
-		// TODO Auto-generated method stub
-		return null;
+		ProductEntity productEntity = null;
+		try {
+			productEntity = repository.findByName(name);
+		} catch (ResourceNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return productEntity.buildDomain();
 	}
 
 	@Override
 	public List<Product> getProduct() {
-		// TODO Auto-generated method stub
-		return null;
+		List<Product> products = new ArrayList<Product>();
+		List<ProductEntity> entities = repository.findAll();
+		for (ProductEntity entity : entities) {
+			Product product = entity.buildDomain();
+			products.add(product);
+		}
+		return products;
 	}
 
 }
